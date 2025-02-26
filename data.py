234b06a69502a3240a5a0a7791ccf70ebf4149f1
younes_qasempour <qasempouryounes@gmail.com>
2025-02-26 23:41:27 +0330

    Finished

--- data.py
+++ data.py
@@ -2,11 +2,12 @@ import requests
 
 API_URL = 'https://opentdb.com/api.php'
 parameters = {
-    'amount': 10,
+    'amount': '10',
     'type': 'boolean'
 }
 
-response = requests.get(API_URL)
+response = requests.get(API_URL, params=parameters)
 response.raise_for_status()
 data = response.json()['results']
 question_data = data
+
